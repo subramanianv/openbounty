9f4e83b5353ba529ac018c8a92dc265e7812c3bb
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2017-10-09 12:18:21 -0400

    WIP

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -23,7 +23,7 @@ import Icon3 from '../images/LandingPage_Icon3.svg';
 class LandingPage extends Component {
 
   redirectToGithub() {
-    window.location.href = "http://github.com/login/oauth/authorize?client_id=79a74ecd4f74e041ef36&redirect_uri=http%3A%2F%2Fopenbounty.co%2Fauth%2Fauthorize?state=1001&scope=user%20user%3Aemail%20user%3Afollow%20public_repo%20repo%20read%3Arepo_hook%20write%3Arepo_hook%20admin%3Arepo_hook";
+    window.location.href = "http://github.com/login/oauth/authorize?client_id=79a74ecd4f74e041ef36&redirect_uri=http%3A%2F%2Fapi.openbounty.co%2Fauth%2Fauthorize?state=1001&scope=user%20user%3Aemail%20user%3Afollow%20public_repo%20repo%20read%3Arepo_hook%20write%3Arepo_hook%20admin%3Arepo_hook";
   }
 
   render() {
